2b8fcaddfbe84b3fb874559c6df6e4f730812bc8
Mohamet Gueye <mohametgueye5@gmail.com>
2025-05-05 16:40:09 +0000

    premiere version 05.05.2025

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+  <ul>
+    <li><a href="#">Menu 1</a></li>
+    <li><a href="#">Menu 2</a></li>
+    <li><a href="#">Menu 3</a></li>
+  </ul>
+</nav>
+    </div>
+  )
+}
+
+export default Header
